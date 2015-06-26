005213e6b6b66f5bd1195d5bfdbf0b3fac997ca1
James Hounshell <jameshounshell@gmail.com>
2015-06-26 17:31:10 -0400

    finished Comparison.java

--- src/jh2pCh2/Comparison.java
+++ src/jh2pCh2/Comparison.java
@@ -20,7 +20,22 @@ public class Comparison
 		System.out.print( "Enter second integer: "); //prompt
 		number2 = input.nextInt(); // read second number from user
 		
+		if ( number1 == number2 )
+			System.out.printf( "%d == %d\n" , number1, number2 );
 		
-	}
-
-}
+		if ( number1 != number2 )
+			System.out.printf( "%d != %d\n", number1, number2 );
+		
+		if ( number1 < number2 )
+			System.out.printf( "%d < %d\n" , number1, number2 );
+		
+		if ( number1 > number2 )
+			System.out.printf( "%d => %d\n" , number1, number2 );
+		
+		if ( number1 <= number2 )
+			System.out.printf( "%d <= %d\n", number1, number2 );
+		
+		if ( number1 >= number2 )
+			System.out.printf( "%d >= %d\n" , number1, number2 );
+	} // end method main
+} // end class comparison
